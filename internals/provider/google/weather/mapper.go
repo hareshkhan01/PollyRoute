f1ef55e05f15eb875d038098cb58bf85302661fd
hareshkhan01 <mrhitman060@gmail.com>
2026-08-26 19:37:34 +0530

    Fix type undefined error in weather mapper.go

--- internals/provider/google/weather/mapper.go
+++ internals/provider/google/weather/mapper.go
@@ -1,21 +1,27 @@
 package weather
 
-func MapResponse(WeatherResponse *response)(*domain.Weather,error){
-	if response==nil{
-		return nil,fmt.Errorf("Response is Null!")
+import (
+	"fmt"
+
+	"github.com/hareshkhan01/PollyRoute/internals/domain"
+)
+
+func MapResponse(response *WeatherResponse) (*domain.Weather, error) {
+	if response == nil {
+		return nil, fmt.Errorf("Response is Null!")
 	}
-	weather:=domain.Weather{
-		Temperature: response.Temperature.Degrees,
-		Visibility: response.Visibility.Distance,
-		RelativeHumidity: response.RelativeHumidity,
-		WindDirection: response.Wind.Direction.Degrees,
-		WindSpeed: response.Wind.Speed.Value,
-		WindGust: response.Wind.Gust.Value,
-		PrecipitationQpf: response.Precipitation.Qpf.Quantity,
-		AirPressure: response.AirPressure.MeanSeaLevelMillibars,
-		WeatherConditionType: response.WeatherCondition.Type,
-		CloudCover: response.CloudCover,
+	weather := domain.Weather{
+		Temperature:             response.Temperature.Degrees,
+		Visibility:              response.Visibility.Distance,
+		RelativeHumidity:        response.RelativeHumidity,
+		WindDirection:           response.Wind.Direction.Degrees,
+		WindSpeed:               response.Wind.Speed.Value,
+		WindGust:                response.Wind.Gust.Value,
+		PrecipitationQpf:        response.Precipitation.Qpf.Quantity,
+		AirPressure:             response.AirPressure.MeanSeaLevelMillibars,
+		WeatherConditionType:    response.WeatherCondition.Type,
+		CloudCover:              response.CloudCover,
 		ThunderstromProbability: response.ThunderstormProbability,
 	}
-	return &weather,nil
+	return &weather, nil
 }
